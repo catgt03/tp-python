b3320fbcef1360879dabb85bd12d8ab112eb12a5
Catalina Gil Tudor <catalinagiltudro@gmail.com>
2026-03-23 18:28:57 -0300

    Muestro mensaje Entrada no válida

--- questions.py
+++ questions.py
@@ -36,19 +36,22 @@ while attempts > 0:
     print(f"Intentos restantes: {attempts}")
     print(f"Letras usadas: {', '.join(guessed)}")
 
-    letter = input("Ingresá una letra: ")
-
-    if letter in guessed:
-        print("Ya usaste esa letra.")
-    elif letter in word:
-        guessed.append(letter)
-        print("¡Bien! Esa letra está en la palabra.")
+    letter = input("Ingresá una letra: ").lower()
+    if len(letter) == 1 and letter.isalpha():
+        if letter in guessed:
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
+        else:
+            guessed.append(letter)
+            attempts -= 1
+            print("Esa letra no está en la palabra.")
     else:
-        guessed.append(letter)
-        attempts -= 1
-        print("Esa letra no está en la palabra.")
-
+        print("Entrada no válida")
+        continue
     print()
 
 else:
     print(f"¡Perdiste! La palabra era: {word}")
+    
